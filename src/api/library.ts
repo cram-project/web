a2b6ac50e28b4ecc9967001f1871f3404a139969
mclowam <max.shabelnikov@gmail.com>
2026-06-23 20:39:31 +0500

    share

--- src/api/library.ts
+++ src/api/library.ts
@@ -1,4 +1,7 @@
-// export default function LibraryPage(){
-//     return (
-//     )
-// }
+import type {LibraryResponse} from "../types/library.ts";
+import api from "./axios.ts";
+
+const library = async ()=>{
+   const r = await api.get<LibraryResponse>("/documents");
+   return r.data
+}
---
+++ src/types/library.ts
@@ -0,0 +1,8 @@
+export interface LibraryResponse {
+    id: string
+    user_id: string
+    title: string
+    created_at: string
+    document_url: string
+}
+
