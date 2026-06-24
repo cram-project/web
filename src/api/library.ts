c4b4e9222c08760521e0232735b681f0e36c238b
mclowam <max.shabelnikov@gmail.com>
2026-06-24 20:59:46 +0500

    docker

--- src/api/library.ts
+++ src/api/library.ts
@@ -1,7 +1,7 @@
 import type {LibraryResponse} from "../types/library.ts";
 import api from "./axios.ts";
 
-const library = async ()=>{
-   const r = await api.get<LibraryResponse>("/documents");
+export const getDocuments = async (): Promise<LibraryResponse[]> => {
+   const r = await api.get<LibraryResponse[]>("/documents");
    return r.data
 }
